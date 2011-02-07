a3e4b9f266f233f63bd73dfe018e7fb9e14ad7f0
unknown <henrik@.(none)>
2011-02-08 00:56:43 +0100

    Commit #10: tested version, needs balancing of servobehaviour and tuning

--- android.c
+++ android.c
@@ -6,7 +6,7 @@
 #define FOSC 10000000
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
-#define BUFFER_SIZE 8
+#define BUFFER_SIZE 9
 
 //function prototypes
 void uart_init(void); //initiating usart communication, setting the correct bits
@@ -18,6 +18,7 @@ static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
 //variables
 volatile char buff[BUFFER_SIZE];
+volatile char ReceivedByte;
 volatile int indx, flag;
 
 
@@ -37,6 +38,7 @@ pwm_init();
 //forever-running loop
 while(1){
 
+
 	servo_parse(); //servo-parsing function
 	
 	
@@ -48,26 +50,20 @@ while(1){
 }
 //end of main
 
-
-
-
-
-
-
-
-
-
-
 //ISR and functions
-
 ISR(USART_RX_vect){
 	
 	
-	buff[indx++] = UDR0; //filling buffer, one byte at a time.
+	if(buff[0] != '%') //check start char, if not equal, reset for another try
+		indx = 0;
+	
+	buff[indx++] = UDR0;//filling buffer, one byte at a time.
+	
 	if(indx>= BUFFER_SIZE){ //when buffer is filled (index bigger or equal to buffer size defined in header)
-	flag = 1;				//set finish flag to 1 to allow reading of datastring, and reset indx to start over
-	indx = 0;				//for new transfer
+		flag = 1;				//set finish flag to 1 to allow reading of datastring, and reset indx to start over
+		indx = 0;				//for new transfer
 	}
+		
 }
 
 
@@ -93,7 +89,7 @@ void uart_init(void){
 
 void pwm_init(void){
 
-	ICR1 = 20000;
+	ICR1 = 12500;
 	TCCR1B |= (1<<WGM13);
 	
 	TCCR1B |= (1<<CS11);
@@ -111,36 +107,45 @@ void servo_parse(void){
 	uint8_t k = 0;
 	uint8_t xDeg, yDeg = 0;
 	char xVal[4], yVal[4];
+	
+		if(buff[4] != '$') //check the data in buffer
+			flag = 0;
+		
+		
 		if(flag==1){ // if new data is available, proceed. 
-		k = 0; 
+			k = 0; 
 		cli(); //disable uart-interrupt, dont want to be interrupted with new data when parsing
 		
 		
-		while(buff[k] != '$'){ //parse through the data until we see the first separator
-			xVal[k] = buff[k];
+		while(buff[k+1] != '$'){ //parse through the data until we see the first separator
+			xVal[k] = buff[k+1];
 			k++;
 		}
 	
 		k=0; //reset the counter
 		
 		
-		while(buff[k+4] != '#'){ //parse through the data until we see the second separator
-			yVal[k] = buff[k+4];
+		while(buff[k+5] != '#'){ //parse through the data until we see the second separator
+			yVal[k] = buff[k+5];
 			k++;
 		}
 		
 	
-	xDeg = atoi(xVal); 
-	yDeg = atoi(yVal);
-	printf("%d - %d", xDeg, yDeg);
 	
-	OCR1A = 900 + xDeg*5;
-	OCR1B = 900 + yDeg*5;
-	 
-	sei(); //enable interrupt, ready for new data 
 	
-	flag = 0; //set flag to false, ready to read new data
-	}
+	
+		xDeg = atoi(xVal); 
+		yDeg = atoi(yVal);
+		//printf("%d - %d", xDeg, yDeg);// for debug purposes
+		
+		OCR1A = 750 + xDeg*2;
+		OCR1B = 750 + yDeg*2;
+		sei(); //enable interrupt, ready for new data 
+		}
+		
+	
+		flag = 0; //set flag to false, ready to read new data
+		
 	
 	
 	
@@ -148,8 +153,7 @@ void servo_parse(void){
 
 
 
-int uart_putchar(char c, FILE *stream)
-{
+int uart_putchar(char c, FILE *stream){
     if (c == '\n') uart_putchar('\r', stream);
   
     loop_until_bit_is_set(UCSR0A, UDRE0);
